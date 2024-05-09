b2e682068557f5ffb0a9d4d86e6874f717705fce
RoderickMFoggia <rodrigomfoggia84@gmail.com>
2024-05-09 04:41:10 -0300

    Data Binding/Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'projectbikeone';
+
+
+  nombre: String = "Sr. Roderick";
+  numero = 101010;
+  numero1 = 808080;
+  resultado = 3333;
+
+  //atajo para identar el codigo : alt + shift + "f"
+  datos: any = {
+    nombre: "Ford",
+    apellido: "Company",
+    edad: 100,
+    documento: "70-100100-50"
+  }
+
+
+
 }
